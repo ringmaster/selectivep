8765d581db406dce228aa814bab033a4a883e402
ringmaster <a_github@midnightcircus.com>
2012-04-09 18:22:18 -0400

    Configure plugin

--- selectivep.plugin.php
+++ selectivep.plugin.php
@@ -3,14 +3,14 @@
 class SelectiveP extends Plugin
 {
 	public function filter_post_content_atom($content, $post) {
-		if($this->should_pee($post->type)) {
+		if($this->should_pee($post->content_type)) {
 			$content = Format::autop($content);
 		}
 		return $content;
 	}
 
 	public function filter_post_content_out($content, $post) {
-		if($this->should_pee($post->type)) {
+		if($this->should_pee($post->content_type)) {
 			$content = Format::autop($content);
 		}
 		return $content;
@@ -24,7 +24,17 @@ class SelectiveP extends Plugin
 	}
 
 	public function should_pee($type) {
+		$types = Options::get('selectivep_types');
+		return in_array($type, $types);
+	}
 
+	public function configure() {
+		$form = new FormUI('selectivep');
+		$options = array_flip(Post::list_active_post_types());
+		unset($options[0]);
+		$form->append(new FormControlCheckboxes('post_types', 'selectivep_types', 'Post types that should autop', $options));
+		$form->append(new FormControlSubmit('save', 'Save'));
+		return $form;
 	}
 }
 
